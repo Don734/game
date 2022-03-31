f6f10fa0634f6dc5df19b12d996e406f817d76ac
Фазилат Нуритдинова <d.rakhimov@ginza.global>
2022-03-31 16:57:07 +0500

    upd game temp

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,30 +1,54 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const swiperContainer = '.swiper-games';
-    const swiperOptions = {
-        // Optional parameters
-        // autoplay: {
-        //     delay: 5000,
-        // },
-      
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            // when window width is >= 320px
-            320: {
-              slidesPerView: 2,
-            },
-            // when window width is >= 480px
-            480: {
-              slidesPerView: 3,
-            },
-            // when window width is >= 640px
-            640: {
-              slidesPerView: 4,
-            }
-          }
-      };
-
-    const swiper = new Swiper(swiperContainer, swiperOptions);
+    gamesSlider();
+    reviewSlider();
 })
+
+function gamesSlider() {
+  const swiperContainer = '.swiper-games';
+  const swiperOptions = {
+      // Optional parameters
+      // autoplay: {
+      //     delay: 5000,
+      // },
+    
+      navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 2,
+          },
+          // when window width is >= 480px
+          480: {
+            slidesPerView: 3,
+          },
+          // when window width is >= 640px
+          640: {
+            slidesPerView: 4,
+          }
+        }
+    };
+
+  const swiper = new Swiper(swiperContainer, swiperOptions);
+}
+
+function reviewSlider() {
+  const swiperContainer = '.game-review .review';
+  const swiperOptions = {
+      // Optional parameters
+      // autoplay: {
+      //     delay: 5000,
+      // },
+      centeredSlides: true,
+      spaceBetween: 30,
+    
+      navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+      },
+    };
+
+  const swiper = new Swiper(swiperContainer, swiperOptions);
+}
